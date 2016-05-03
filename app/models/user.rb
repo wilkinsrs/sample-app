95af42312686c0558ec3f29cc59d56433c3ab4fe
Ryan <wilkinsrs@outlook.com>
2016-05-03 00:49:52 +0000

    create user model validation and associated tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+    #another way to write these: validates(:name, presence: true)
+    before_save { self.email = self.email.downcase } #self is optional on the right side of equation
+    
+    validates :name,    presence: true,
+                        length: { maximum: 50 }
+                        
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
+    validates :email,   presence: true,
+                        length: { maximum: 255 }, 
+                        format: { with: VALID_EMAIL_REGEX }, 
+                        uniqueness: { case_sensitive: false } #rails infers that uniquness is true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
+#build out validation logic checks here, but actually create the validation code in the model
+#for each test, adjust the instance variable for the test
+
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup #set up an instance variable for all tests
+    @user = User.new(:name => "Example User", :email => "user@example.com")
+  end
+  
+  test "should be valid" do #test that the instance variable is valid
+    assert @user.valid? #assert 'passes' if the .valid method returns true, and fails if it returns false
+  end
+  
+  test "name should be present" do #test presence of a name
+    @user.name = "" #the below method says 'don't assert that this is true'
+    assert_not @user.valid? #assert 'passes' if .valid? method fails and it fails if it returns true
+  end
+  
+  test "email should be present" do #test presence of email
+    @user.email = ""
+    assert_not @user.valid?
+  end
+  
+  test "name is less than 51 characters" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email is less than 51 characters" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email is proper format" do
+    valid_addresses = %w[foobar@example.com USER@example.com foo.b-ar@example.com alicebob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+  test "email is not invalid format" do
+    invalid_addresses = %w[foobar_at_example.com foobar@example,com user@baz_baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} is not valid"
+    end
+  end
+  
+  test "email is not a duplicate" do
+    duplicate_user = @user.dup #creaets a duplicate user of @user
+    duplicate_user.email = @user.email.upcase #makes email all upcase
+    @user.save #saves @user to the db
+    assert_not duplicate_user.valid? #asserts that duplicate_user isn't valid because @user.email already exists in db
+  end #notice that .valid? checks the db automatically
+    
+  
 end
