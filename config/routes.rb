d371099e09cd4922d1df9f596d5b51e0f0291024
Ryan <wilkinsrs@outlook.com>
2016-05-19 19:52:51 +0000

    add show view for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
   get 'signup'  =>  'users#new' #started off as get 'users/new' but can rename route this way
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
